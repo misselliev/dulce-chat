60ff93987611c512d3f5db9486087326d29d7ade
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-07-09 19:10:45 -0700

    adding authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :logged_in?
 
   def current_user
-    @current_user || = User.find(Session[:user_id]) if session[user_id]
+    @current_user ||= User.find(Session[:user_id]) if session[user_id]
   end
 
   def logged_in?
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,22 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success]="You are logged in!"
+      redirect_to root_path
+    else
+      flash.now[:error]="There was an error with your information"
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] ="You have logged out!"
+    redirect_to login_path
+  end
 end

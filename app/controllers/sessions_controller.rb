cbb03bc1196d871ccd4083ffcbd1052357edf973
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-07-15 16:20:36 -0700

    Fixing session username error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,10 @@ class SessionsController < ApplicationController
     user = User.find_by(username: params[:session][:username])
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      flash[:success]="You are logged in!"
+      flash[:success] = "You have successfully logged in"
       redirect_to root_path
     else
-      flash.now[:error]="There was an error with your information"
+      flash.now[:error] = "There was something wrong with your login information"
       render 'new'
     end
   end

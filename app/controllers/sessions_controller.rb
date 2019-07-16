ff790eb004522b25962b2fe6a880297556b8cc1d
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-07-15 17:00:19 -0700

    adding restrictions to index and login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  before_action :logged_in_redirect, only: [:new, :create]
   def new
   end
 
@@ -19,4 +20,14 @@ class SessionsController < ApplicationController
     flash[:success] ="You have logged out!"
     redirect_to login_path
   end
+
+private
+
+def logged_in_redirect
+  if logged_in?
+    flash[:error]="You're already logged in"
+    redirect_to root_path
+  end
+end
+
 end
